14f65b1cd4e48803a4085cad0c38e87da7ac1e50
danielarjordao <danielarjordao@hotmail.com>
2026-05-24 16:01:07 +0100

    refactor: comment out Header component and update routing configuration in app files

--- meu-projeto/src/app/app.routes.ts
+++ meu-projeto/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Header } from './header/header';
+import { Posts } from './posts/posts';
+import { Auth } from './auth/auth';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: '', component: Header },
+	{ path: 'header', component: Header },
+	{ path: 'posts', component: Posts },
+	{ path: 'auth', component: Auth },
+];
